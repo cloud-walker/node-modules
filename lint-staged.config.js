349d4e950d0649a5a74c6d44db5db2dac19d973b
Luca Barone <baro.luc@gmail.com>
2020-01-01 04:40:06 +0100

    Prettify all the things

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "*.{json,yaml,ts}": ["prettier --write", "git add"],
-  "*.js": ["prettier --write", "git add"]
-};
+  '*.{json,yaml,ts}': ['prettier --write', 'git add'],
+  '*.js': ['prettier --write', 'git add'],
+}
